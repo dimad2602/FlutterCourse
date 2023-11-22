892c41905c135e7b6e791dc677458ac606d9345e
dimad2602 <dimad26002@gmail.com>
2023-11-22 20:30:58 +0700

    pr20 Row и Column

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:curse_app_1/components/big_text.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -37,34 +38,60 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: Stack(
-              alignment: Alignment.bottomLeft,
-              children: [
-                Container(
-                width: 300,
-                height: 300,
-                color: Colors.red,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Container(
+          height: 100,
+          width: 300,
+          decoration: BoxDecoration(
+              border: Border.all(
+            color: Colors.grey[200]!,
+          )),
+          child: Row(
+            children: [
+              Column(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.all(16),
+                    width: 64,
+                    height: 64,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[400],
+                      shape: BoxShape.circle,
+                    ),
+                  )
+                ],
               ),
-              Container(
-                width: 200,
-                height: 150,
-                color: Colors.blue,
-              ),
-              Container(
-                margin: const EdgeInsets.all(16),
-                width: 100,
-                height: 100,
-                color: const Color.fromARGB(255, 37, 254, 25),
-              ),
-              ],
-            ),
-          )
-        ],
+              const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    children: [
+                      BigText(
+                        text: "Имя",
+                        size: 20,
+                        fontWeight: FontWeight.w500,
+                      ),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      BigText(
+                        text: "Фамилия",
+                        size: 20,
+                        fontWeight: FontWeight.w500,
+                      )
+                    ],
+                  ),
+                  Text(
+                    'Должность',
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
