5225e59f660c370e82b4a8d3048e00467637e2b1
dimad2602 <dimad26002@gmail.com>
2023-11-22 22:20:38 +0700

    pr22 GridView

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:curse_app_1/widgets/visit_card_widget.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -34,16 +33,31 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: ListView.builder(
-        itemCount: 100,
-        itemBuilder: (context, index) {
-          return const VisitCardWidget();
-        },
-      ),
-    );
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            mainAxisSpacing: 5.0,
+            mainAxisExtent: 25,
+          ),
+          itemBuilder: (context, index) {
+            int row = 1 + index ~/ 3;
+            int col = index % 3;
+            int number = row + col;
+            return Center(
+              child: Text(
+                number.toString(),
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 20.0,
+                ),
+              ),
+            );
+          },
+          itemCount: 3 * 6,
+        ));
   }
 }
--- lib/widgets/visit_card_widget.dart
+++ lib/widgets/visit_card_widget.dart
@@ -6,59 +6,56 @@ class VisitCardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Container(
-        height: 100,
-        width: 300,
-        decoration: BoxDecoration(
-            border: Border.all(
-          color: Colors.grey[200]!,
-        )),
-        child: Row(
-          children: [
-            Column(
-              children: [
-                Container(
-                  margin: const EdgeInsets.all(16),
-                  width: 64,
-                  height: 64,
-                  decoration: BoxDecoration(
-                    color: Colors.grey[400],
-                    shape: BoxShape.circle,
-                  ),
-                )
-              ],
-            ),
-            const Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Row(
-                  children: [
-                    BigText(
-                      text: "Имя",
-                      size: 20,
-                      fontWeight: FontWeight.w500,
-                    ),
-                    SizedBox(
-                      width: 8,
-                    ),
-                    BigText(
-                      text: "Фамилия",
-                      size: 20,
-                      fontWeight: FontWeight.w500,
-                    )
-                  ],
+    return Container(
+      height: 100,
+      width: 300,
+      decoration: BoxDecoration(
+          border: Border.all(
+        color: Colors.grey[200]!,
+      )),
+      child: Row(
+        children: [
+          Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.all(16),
+                width: 64,
+                height: 64,
+                decoration: BoxDecoration(
+                  color: Colors.grey[400],
+                  shape: BoxShape.circle,
                 ),
-                Text(
-                  'Должность',
-                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
-                )
-              ],
-            )
-          ],
-        ),
+              )
+            ],
+          ),
+          const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                children: [
+                  BigText(
+                    text: "Имя",
+                    size: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  BigText(
+                    text: "Фамилия",
+                    size: 20,
+                    fontWeight: FontWeight.w500,
+                  )
+                ],
+              ),
+              Text(
+                'Должность',
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
+              )
+            ],
+          )
+        ],
       ),
     );
   }
